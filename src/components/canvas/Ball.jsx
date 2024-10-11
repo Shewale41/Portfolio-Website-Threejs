a61947d2185dfc0fef9f625a3dc5c302f232be0b
ethanw2457 <130408385+ethanw2457@users.noreply.github.com>
2024-10-10 20:54:11 -0400

    Ball Canvas

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import {
+  Decal, Float, OrbitControls, Preload, useTexture
+} from '@react-three/drei'
 
-const Ball = () => {
+import CanvasLoader from '../Loader';
+// Change the values of the balls in index.js an search up: const technologies
+const Ball = (props) => {
+  const [decal] = useTexture([props.imgUrl]);
+
   return (
     <div>Ball</div>
+  )
+}
+
+const BallCanvas = ({ icon }) => {
+  return (
+    <Canvas
+      frameloop='demand'
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControlsenable Zoom={false} />
+        <Ball imgUrl={icon} />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
   )
 }
 
