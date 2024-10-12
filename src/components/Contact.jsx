12b7858c59d0dded797886eeaccc421cb2abb950
ethanw2457 <130408385+ethanw2457@users.noreply.github.com>
2024-10-12 00:29:48 -0400

    email input

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -35,6 +35,28 @@ const Contact = () => {
           className="mt-12 flex flex-col gap-8"
         >
           <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Email</span>
+            <input 
+              type="email"
+              name="email"
+              value={form.email}
+              onChange={handleChange}
+              placeholdeer="What's your email?"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium"
+            />
+          </label>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Name</span>
+            <input 
+              type="text"
+              name="name"
+              value={form.name}
+              onChange={handleChange}
+              placeholdeer="What's your name?"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium"
+            />
+          </label>
+          <label className="flex flex-col">
             <span className="text-white font-medium mb-4">Your Name</span>
             <input 
               type="text"
