683d2082dff08be2f5d3b530a2d9b0bf841d4dff
ethanw2457 <130408385+ethanw2457@users.noreply.github.com>
2024-10-12 00:05:53 -0400

    Set up contact foundations

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,9 +8,23 @@ import { SectionWrapper } from '../hoc';
 import { slideIn } from '../utils/motion';
 
 const Contact = () => {
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {}
+
+  const handleSubmit = (e) => {}
+
   return (
-    <div>Contact</div>
+    <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
+
+    </div>
   )
 }
 
-export default SectionWrapper(Contact, "")
+export default SectionWrapper(Contact, "contact")
